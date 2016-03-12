006e754e72bfc8878a0c07ecf8cf6c90f4dafdd3
fgq <fgq2010@126.com>
2016-03-11 18:47:00 -0800

    1、创建最初始化的xcodeproject,将viewcontroller换成UITableviewController

---
+++ todo/todo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  todo
+//
+//  Created by IMAC on 16/3/11.
+//  Copyright © 2016年 IMAC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UITableViewController
+
+
+@end
+
---
+++ todo/todo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  todo
+//
+//  Created by IMAC on 16/3/11.
+//  Copyright © 2016年 IMAC. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
